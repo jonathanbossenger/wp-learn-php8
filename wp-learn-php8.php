81360ab780c0a4ce65d16ebe825ec12be60d9687
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-05-11 12:22:19 +0200

    Moving posts functionality to a class

--- wp-learn-php8.php
+++ wp-learn-php8.php
@@ -5,20 +5,33 @@
  * Version: 1.0.0
  */
 
+class post_fetcher {
+
+	protected $posts;
+
+	public function post_fetcher() {
+		$this->posts     = get_posts();
+	}
+
+	public function fetch_posts() {
+		$post_html = '<div class="post">';
+		foreach ( $this->posts as $post ) {
+			if ( array_key_exists( 'post_title', $post ) ) {
+				$post_html .= sprintf(
+					'<h4><a href="%s">%s</a></h4>',
+					get_permalink( $post->ID ),
+					$post->post_title
+				);
+			}
+		}
+		$post_html .= '</div>';
+		return $post_html;
+	}
+}
+
 add_shortcode( 'wp_learn_php8', 'wp_learn_php8_shortcode_render' );
 function wp_learn_php8_shortcode_render() {
-	$posts     = get_posts();
-	$post_html = '<div class="post">';
-	foreach ( $posts as $post ) {
-		if ( array_key_exists( 'post_title', $post ) ) {
-			$post_html .= sprintf(
-				'<h4><a href="%s">%s</a></h4>',
-				get_permalink( $post->ID ),
-				$post->post_title
-			);
-		}
-	}
-	$post_html .= '</div>';
-
+	$post_fetcher = new post_fetcher();
+	$post_html = $post_fetcher->fetch_posts();
 	return $post_html;
 }
